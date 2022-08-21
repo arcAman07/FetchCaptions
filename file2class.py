83924c6e9d34454f6c353aad54bf2d328ad41a72
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2022-08-22 02:30:28 +0530

    Add files via upload

---
+++ file2class.py
@@ -0,0 +1,10 @@
+FILE_TO_CLASS = {
+    'LSTMDecoderWords.best.pth': CaptioningLSTM,
+    'LSTMDecoderChars.best.pth': CaptioningLSTM,
+    'LSTMDecoderWithLabelsWords.best.pth': CaptioningLSTMWithLabels,
+    'LSTMDecoderWithLabelsChars.best.pth': CaptioningLSTMWithLabels,
+    'TransformerDecoderBaseWords.best.pth': CaptioningTransformerBase,
+    'TransformerDecoderBaseChars.best.pth': CaptioningTransformerBase,
+    'TransformerDecoderWords.best.pth': CaptioningTransformer,
+    'TransformerDecoderChars.best.pth': CaptioningTransformer
+}
